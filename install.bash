cea5eb193f47fc206b27957f56305112754ce225
Michael Snowden <michael.snowden@glassdoor.com>
2015-10-04 00:40:08 -0700

    Added a master install script

---
+++ install.bash
@@ -0,0 +1,20 @@
+git clone https://github.com/DakotaAndMichael/eesubmit.git
+cd eesubmit/main
+echo "What is your OS?"
+echo "(1) Mac"
+echo "(2) Linux"
+echo "(3) Windows"
+echo "(4) Other"
+
+read os
+
+case $os in
+    1 )
+        ./macInstall.bash ;;
+    2 )
+        ./linuxInstall.bash ;;
+    3 )
+        echo "Windows is not yet supported." ;;
+    4 )
+        echo "Other operating systems are not yet supported." ;;
+esac
